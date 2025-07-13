fa9be62264245449d5a752c9dbf91549bc7c2297
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 22:12:46 -0400

    feat: define the main app layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang='ts'>
+    import type { Snippet } from 'svelte';
+
     import '../app.css';
 
-    const { children } = $props();
+    import { Navbar } from '$lib/components/ui';
+
+    interface Props {
+        children: Snippet;
+    }
+
+    const { children }: Props = $props();
 </script>
 
-{@render children()}
+<div class='grid min-w-88 grid-rows-[auto_1fr_auto]'>
+    <Navbar />
+
+    <main class='relative min-h-[calc(100dvh-var(--ui-header-height))]'>
+        {@render children()}
+    </main>
+</div>
