0afcb3785ba5f31611de60139fa2642efedff0a2
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-12 22:12:12 -0400

    feat: added hero component

---
+++ src/lib/components/ui/blocks/hero/hero.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+</script>
+
+<section class='size-full'>
+    <div class='grid min-h-200 grid-cols-2'>
+        <div>
+            <h1 class='text-3xl font-semibold'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </h1>
+            <span
+            >Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Pariatur, animi, enim libero dolores necessitatibus dolorum
+                quidem mollitia, tenetur id sint itaque cumque a? Nulla iusto
+                blanditiis minima fugit cumque minus!</span
+            >
+        </div>
+        <div class='bg-separator-light'></div>
+    </div>
+</section>
