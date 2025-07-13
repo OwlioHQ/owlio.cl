23778452ee824d5e633f61c08efacb45a32161e9
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-13 18:53:06 -0400

    feat: added collapsible component

---
+++ src/lib/components/ui/collapsible/collapsible.svelte
@@ -0,0 +1,25 @@
+<script lang='ts'>
+    import type { Snippet } from 'svelte';
+    import type { HTMLAttributes } from 'svelte/elements';
+
+    import { cn } from '$lib/utils';
+    import { slide } from 'svelte/transition';
+
+    interface Props extends HTMLAttributes<HTMLElement> {
+        open: boolean;
+        children: Snippet;
+    }
+
+    let {
+        open = $bindable(false),
+        children,
+        class: class_name,
+        ...rest
+    }: Props = $props();
+</script>
+
+{#if open}
+    <div class={cn('', class_name)} {...rest} transition:slide={{ axis: 'y' }}>
+        {@render children()}
+    </div>
+{/if}
