ef405755e3dd6898fa48aeb3a959f240d669a9c3
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-16 22:39:46 -0400

    refactor: update exports

---
+++ src/lib/components/blocks/index.ts
@@ -0,0 +1,5 @@
+export { Faq } from './faq';
+export { Hiw } from './hiw';
+export { Cta } from './cta';
+export { Hero } from './hero';
+export { Features } from './features';
--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -1,4 +1,8 @@
+export { Card } from './card';
 export { Navbar } from './navbar';
 export { Footer } from './footer';
+export { Button } from './button';
+export { IconBadge } from './icon-badge';
 export { Collapsible } from './collapsible';
+export { FeatureItem } from './feature-item';
 export { Accordion, AccordionGroup } from './accordion';
