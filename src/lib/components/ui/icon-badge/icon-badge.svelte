80ddc57f1037b5825a39596da14af41705214529
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-16 22:26:36 -0400

    feat: added icon badge component

---
+++ src/lib/components/ui/icon-badge/icon-badge.svelte
@@ -0,0 +1,22 @@
+<script lang='ts'>
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import { cn } from '$lib/utils';
+
+	interface Props extends HTMLAttributes<HTMLElement> {
+		children: Snippet;
+	}
+
+	let { children, class: class_name, ...rest }: Props = $props();
+</script>
+
+<div
+	class={cn(
+		'aspect-square size-16 content-center rounded-full bg-primary-600 *:mx-auto *:size-1/2 *:align-middle *:text-background-primary',
+		class_name,
+	)}
+	{...rest}
+>
+	{@render children()}
+</div>
