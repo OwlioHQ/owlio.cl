6fd231821ebabe051b041648dc3b68f17b0160e2
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-16 22:39:30 -0400

    feat: added new sections

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,45 @@
 <script lang='ts'>
-    import type { Snippet } from 'svelte';
-    import type { NavItem } from '$lib/types';
+	import type { Snippet } from 'svelte';
+	import type { NavItem } from '$lib/types';
 
-    import '../app.css';
+	import '../app.css';
 
-    import { Navbar, Footer } from '$lib/components/ui';
+	import { Navbar, Footer } from '$lib/components/ui';
 
-    interface Props {
-        children: Snippet;
-    }
+	interface Props {
+		children: Snippet;
+	}
 
-    const { children }: Props = $props();
+	const { children }: Props = $props();
 
-    const items: NavItem[] = [
-        {
-            label: 'home',
-            href: '#',
-        },
-        {
-            label: 'features',
-            href: '#features',
-        },
-        {
-            label: 'faq',
-            href: '#faq',
-        },
-    ];
+	const items: NavItem[] = [
+		{
+			label: 'inicio',
+			href: '#inicio',
+		},
+		{
+			label: 'servicios',
+			href: '#servicios',
+		},
+		{
+			label: 'como trabajamos',
+			href: '#hiw',
+		},
+		{
+			label: 'preguntas frecuentes',
+			href: '#faq',
+		},
+	];
 </script>
 
 <div
-    class='grid min-w-88 grid-rows-[auto_1fr_auto] bg-background-primary text-text-primary'
+	class='grid min-w-88 grid-rows-[auto_1fr_auto] bg-background-primary text-text-secondary'
 >
-    <Navbar {items} />
+	<Navbar {items} />
 
-    <main class='relative min-h-[calc(100dvh-var(--header-height))]'>
-        {@render children()}
-    </main>
+	<main class='relative min-h-[calc(100dvh-var(--header-height))]'>
+		{@render children()}
+	</main>
 
-    <Footer {items} />
+	<Footer {items} />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang='ts'>
-    import { Hero, Features, Faq } from '$lib/components/ui/blocks';
+	import { Hero, Features, Faq, Hiw, Cta } from '$lib/components/blocks';
 </script>
 
 <div>
-    <Hero id='home' />
-    <Features id='features' />
-    <Faq id='faq' />
+	<Hero id='inicio' class='scroll-m-20' />
+	<Features id='servicios' class='scroll-m-10' />
+	<Hiw id='hiw' class='scroll-m-32' />
+	<Faq id='faq' class='scroll-m-20' />
+	<Cta id='cta' />
 </div>
