fe6a2afafd75176177ea65098860366706306f97
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-24 21:29:38 -0400

    feat: added logo slider component

--- src/lib/components/ui/index.ts
+++ src/lib/components/ui/index.ts
@@ -3,6 +3,7 @@ export { Navbar } from './navbar';
 export { Footer } from './footer';
 export { Button } from './button';
 export { IconBadge } from './icon-badge';
+export { LogoSlider } from './logo-slider';
 export { Collapsible } from './collapsible';
 export { FeatureItem } from './feature-item';
 export { Accordion, AccordionGroup } from './accordion';
