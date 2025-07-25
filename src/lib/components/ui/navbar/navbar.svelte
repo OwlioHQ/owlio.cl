841095ff6fb376e5c8c5a159784db06f34e5c576
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-24 21:29:22 -0400

    feat: change style when passed the hero section

--- src/lib/components/ui/navbar/navbar.svelte
+++ src/lib/components/ui/navbar/navbar.svelte
@@ -8,47 +8,70 @@
 	}
 
 	let { items }: Props = $props();
+
+	let passed_hero = $state<boolean>(false);
+
+	function observe_hero() {
+		const target = document.getElementById('inicio');
+
+		if (!target)
+			return;
+
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				passed_hero = !entry.isIntersecting;
+			},
+			{
+				root: null,
+				threshold: 0.03,
+			},
+		);
+
+		observer.observe(target);
+
+		return {
+			destroy() {
+				observer.disconnect();
+			},
+		};
+	}
+
+	$effect(() => {
+		observe_hero();
+	});
 </script>
 
-<header class='sticky top-0 z-50 w-full bg-background-primary'>
-	<div
-		class='relative border-b border-b-separator px-[var(--section-padding-x)]'
-	>
+<header class={`fixed top-0 z-50 w-full shadow backdrop-blur-2xl ${passed_hero ? 'bg-white/40 text-text-primary' : 'bg-gradient-to-r from-indigo-950/20 to-secondary-950/20 text-background-primary/90'}`}>
+	<div class='relative px-[var(--section-padding-x)]'>
 		<nav class='flex h-[var(--header-height)] justify-between gap-x-16'>
 			<div class='flex items-center gap-6'>
-				<a class='flex items-center gap-2' href='/'>
-					<span
-						class='text-xl tracking-widest text-text-secondary uppercase'
-					>
-						<span class='font-semibold text-text-primary'
-						>Rapid</span
-						>flow
-					</span>
-				</a>
-				<div class='flex items-center'>
-					<!--  -->
+				<div class='pr-10'>
+					<a class='flex items-center gap-2' href='/'>
+						<span
+							class='text-xl font-bold tracking-widest uppercase'
+						>
+							Owl<span class='font-light'>io</span>
+						</span>
+					</a>
+				</div>
+				<div class='flex items-center pl-4'>
+					{#if items}
+						<ul class='flex items-center gap-x-8 font-medium capitalize'>
+							{#each items as item (item.label)}
+								<li>
+									<a
+										clasS='block cursor-pointer p-1 whitespace-nowrap'
+										href={item.href}>{item.label}</a
+									>
+								</li>
+							{/each}
+						</ul>
+					{/if}
 				</div>
 			</div>
 
-			<div class='flex items-center'>
-				{#if items}
-					<ul class='flex items-center gap-x-8'>
-						{#each items as item (item.label)}
-							<li class='text-text-secondary'>
-								<a
-									clasS='block cursor-pointer p-1 whitespace-nowrap'
-									href={item.href}>{item.label}</a
-								>
-							</li>
-						{/each}
-					</ul>
-				{/if}
-			</div>
-
 			<div class='flex items-center gap-x-4'>
-				<Button>
-					Lorem ipsum
-				</Button>
+				<Button>Contáctanos</Button>
 			</div>
 		</nav>
 	</div>
