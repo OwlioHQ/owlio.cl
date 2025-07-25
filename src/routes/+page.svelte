e602409c266fc094d7e9568043f023616b4d527f
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-24 21:12:47 -0400

    fix: adjust scroll margin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,8 @@
 
 <div>
 	<Hero id='inicio' class='scroll-m-20' />
-	<Features id='servicios' class='scroll-m-10' />
-	<Hiw id='hiw' class='scroll-m-32' />
-	<Faq id='faq' class='scroll-m-20' />
+	<Features id='servicios' />
+	<Hiw id='hiw' class='scroll-m-36' />
+	<Faq id='faq' />
 	<Cta id='cta' />
 </div>
