08ccb4f1616a1f05eb0371a045382cb3c79311e5
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-07-31 15:52:17 -0400

    feat: added SEO

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,14 @@
 	import { Hero, Cta, Features, Hiw, Faq } from '$lib/components/blocks';
 </script>
 
+<svelte:head>
+	<title>Owlio</title>
+	<meta
+		name='description'
+		content='Transformamos empresas a través de soluciones digitales personalizadas. Nos especializamos en desarrollo de software, automatización de procesos y optimización operacional para ayudar a tu negocio a modernizarse y crecer de manera más eficiente.'
+	/>
+</svelte:head>
+
 <div>
 	<Hero id='inicio' class='scroll-m-20' />
 	<Features id='servicios' />
