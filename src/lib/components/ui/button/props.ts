d7b4604a7212d85a629b0b01b50b16f3d80b4caa
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-08-12 17:27:25 -0400

    feat: added button variants

--- src/lib/components/ui/button/props.ts
+++ src/lib/components/ui/button/props.ts
@@ -1,1 +1,35 @@
-export const base_style = 'h-10 flex items-center gap-x-2 w-fit px-4 rounded-md bg-primary-600 text-white cursor-pointer text-base font-medium text-center whitespace-nowrap';
+export const base_style = 'outline-none rounded-md flex items-center justify-center gap-x-2 cursor-pointer font-medium text-base whitespace-nowrap transition-colors duration-300';
+
+export const variants = {
+	primary:
+		'bg-primary text-primary-foreground hover:bg-primary/90 focus-within:bg-primary/90 focus-within:ring-2 focus-within:ring-primary focus-within:ring-offset-2 focus-within:ring-offset-background',
+	secondary:
+		'bg-secondary text-secondary-foreground hover:bg-secondary/90 focus-within:bg-secondary/90 focus-within:ring-2 focus-within:ring-secondary focus-within:ring-offset-2 focus-within:ring-offset-background',
+	accent:
+		'bg-accent text-accent-foreground hover:bg-accent/90 focus-within:bg-accent/90 focus-within:ring-2 focus-within:ring-accent focus-within:ring-offset-2 focus-within:ring-offset-background',
+	muted:
+		'bg-muted text-muted-foreground hover:bg-muted/90 focus-within:bg-muted/90 focus-within:ring-2 focus-within:ring-muted focus-within:ring-offset-2 focus-within:ring-offset-background',
+	destructive:
+		'bg-destructive text-destructive-foreground hover:bg-destructive/90 focus-within:bg-destructive/90 focus-within:ring-2 focus-within:ring-destructive focus-within:ring-offset-2 focus-within:ring-offset-background',
+	outline:
+		'border border-foreground text-foreground bg-transparent hover:bg-foreground/10 focus-within:bg-foreground/10 focus-within:ring-2 focus-within:ring-foreground focus-within:ring-offset-2 focus-within:ring-offset-background',
+	ghost:
+		'bg-transparent text-foreground hover:bg-foreground/10 focus-within:bg-foreground/10 focus-within:ring-2 focus-within:ring-foreground focus-within:ring-offset-2 focus-within:ring-offset-background',
+	glass:
+		'bg-foreground/20 backdrop-blur-md text-foreground hover:bg-foreground/30 focus-within:bg-foreground/30 focus-within:ring-2 focus-within:ring-foreground focus-within:ring-offset-2 focus-within:ring-offset-background',
+};
+
+export const sizes = {
+	sm: 'h-8 px-4 text-sm',
+	md: 'h-10 px-6 text-base',
+	lg: 'h-12 px-8 text-lg',
+	icon: 'size-10 rounded-full aspect-square',
+};
+
+type ButtonVariants = keyof typeof variants;
+type ButtonSizes = keyof typeof sizes;
+
+export type ButtonOptions = Partial<{
+	variant: ButtonVariants;
+	size: ButtonSizes;
+}>;
