fc0dfa62e50e06bd6ad075c3272d2b128d54f2c1
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-08-12 17:29:02 -0400

    feat: move constants to its own file

--- src/lib/components/ui/footer/footer.svelte
+++ src/lib/components/ui/footer/footer.svelte
@@ -1,78 +1,49 @@
 <script lang='ts'>
 	import type { HTMLAttributes } from 'svelte/elements';
 
-	import XLogo from 'phosphor-svelte/lib/XLogo';
-	import FacebookLogo from 'phosphor-svelte/lib/FacebookLogo';
-	import LinkedinLogo from 'phosphor-svelte/lib/LinkedinLogo';
 	import OwlioLogo from '$lib/assets/images/owlio.png?enhanced';
-	import InstagramLogo from 'phosphor-svelte/lib/InstagramLogo';
-	import EnvelopeSimple from 'phosphor-svelte/lib/EnvelopeSimple';
 
 	import { cn } from '$lib/utils';
+	import { socials } from '$lib/constants';
+
+	import { Link } from '../link';
 
 	interface Props extends HTMLAttributes<HTMLElement> {}
 
 	let { class: class_name, ...rest }: Props = $props();
-
-	const socials = [
-		{
-			label: 'Mail',
-			icon: EnvelopeSimple,
-			href: 'mailto:owlio.contacto@gmail.com',
-		},
-		{
-			label: 'X',
-			icon: XLogo,
-			href: 'https://x.com/owlio_cl',
-		},
-		{
-			label: 'Instagram',
-			icon: InstagramLogo,
-			href: 'https://www.instagram.com/owlio_cl/?hl=es',
-		},
-		{
-			label: 'Facebook',
-			icon: FacebookLogo,
-			href: 'https://web.facebook.com/profile.php?id=61578924622725',
-		},
-		{
-			label: 'Linkedin',
-			icon: LinkedinLogo,
-			href: 'https://www.linkedin.com/company/owlio-cl',
-		},
-	];
 </script>
 
 <footer
-	class={cn(
-		'bg-background-primary px-[var(--section-padding-x)]',
-		class_name,
-	)}
+	class={cn('mx-auto w-full max-w-(--content-width) bg-background px-(--content-x-spacing)', class_name)}
 	{...rest}
 >
-	<div class='divide-y divide-separator'>
+	<div class='divide-y divide-muted-foreground'>
 		<div class='flex items-center justify-between py-8'>
 			<a class='flex items-center gap-2' href='/'>
 				<div class='flex items-center gap-x-2'>
-					<enhanced:img class='w-4 object-contain' alt='Owlio logo' src={OwlioLogo} />
-					<span
-						class='text-xl font-bold tracking-widest text-text-primary uppercase'
-					>
-						Owl<span class='font-light text-text-secondary'>io</span>
+					<enhanced:img
+						class='w-4 object-contain'
+						alt='Owlio logo'
+						src={OwlioLogo}
+					/>
+					<span class='text-xl font-bold tracking-widest uppercase'>
+						Owlio
 					</span>
 				</div>
 			</a>
 		</div>
 		<div class='flex items-center justify-between py-8'>
-			<span class='text-sm text-text-secondary'>
-				© 2025 Owlio. All rights reserved.
+			<span class='text-sm text-muted'>
+				© 2025 Owlio.
 			</span>
 			<div class='flex items-center gap-x-3'>
 				{#each socials as social}
 					{@const Logo = social.icon}
-					<a aria-label={social.label} href={social.href} referrerpolicy='no-referrer' target='_blank'>
-						<Logo class='size-6 transition-colors focus-within:fill-black hover:fill-black' />
-					</a>
+					<Link aria-label={social.label} href={social.href}>
+						<Logo
+							class='size-6 fill-foreground transition-colors group-focus-within:fill-accent group-hover:fill-accent'
+						/>
+					</Link>
 				{/each}
 			</div>
 		</div>
