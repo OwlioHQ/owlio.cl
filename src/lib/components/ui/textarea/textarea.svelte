a4bbd4aaf6b9168651f347d455b44d868cf4d52b
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-08-14 16:12:05 -0400

    feat: added textarea component

---
+++ src/lib/components/ui/textarea/textarea.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+	import type { HTMLTextareaAttributes } from 'svelte/elements';
+
+	import { cn } from '$lib/utils';
+
+	interface Props extends HTMLTextareaAttributes {}
+
+	let { value = $bindable(), class: class_name, ...rest }: Props = $props();
+</script>
+
+<textarea
+	class={cn(
+		'block w-full resize-none rounded-md border border-muted bg-background px-3 py-1.5 text-sm ring-offset-background placeholder:text-muted read-only:cursor-not-allowed read-only:bg-muted read-only:text-muted read-only:ring-offset-muted read-only:select-none focus-within:ring-2 focus-within:ring-primary focus-within:ring-offset-2 focus-within:outline-none',
+		class_name,
+	)}
+	rows={3}
+	{...rest}
+	bind:value
+></textarea>
