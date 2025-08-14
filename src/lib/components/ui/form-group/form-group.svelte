39d4a436857cc510fc74d1441dd3c0c96a306024
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-08-14 16:13:42 -0400

    feat: added form group component

---
+++ src/lib/components/ui/form-group/form-group.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import { cn } from '$lib/utils';
+
+	interface Props extends HTMLAttributes<HTMLElement> {
+		children: Snippet;
+	}
+
+	const { children, class: class_name, ...rest }: Props = $props();
+</script>
+
+<div class={cn('space-y-2 rounded-md py-3', class_name)} {...rest}>
+	{@render children()}
+</div>
