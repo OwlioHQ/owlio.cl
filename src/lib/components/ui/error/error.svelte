4446aba922b4b66bba59306451e9fd84d9ffd277
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-08-14 16:13:24 -0400

    feat: added error component

---
+++ src/lib/components/ui/error/error.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import { cn } from '$lib/utils';
+
+	interface Props extends HTMLAttributes<HTMLElement> {
+		children: Snippet;
+	}
+
+	let { children, class: class_name, ...rest }: Props = $props();
+</script>
+
+<small class={cn('-mt-1 block pl-1.5 text-xs text-destructive', class_name)} {...rest}>
+	{@render children()}
+</small>
