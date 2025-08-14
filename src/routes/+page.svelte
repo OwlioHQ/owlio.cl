830cf92adbdda6df84417cb87828f32f9bd7f021
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-08-14 16:11:46 -0400

    feat: added contact section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,52 @@
 	import CatGif from '$lib/assets/gifs/cat.gif';
 	import HighwayGif from '$lib/assets/gifs/highway.gif';
 
+	import { Toaster, toast } from 'svelte-sonner';
 	import { ArrowRight } from '$lib/components/icons';
-	import { features, hero_card_items, questions, steps } from '$lib/constants';
-	import { Accordion, AccordionGroup, Button, Card, FeatureItem, LogoSlider } from '$lib/components/ui';
+	import {
+		company_size_options,
+		contact_items,
+		features,
+		hero_card_items,
+		questions,
+		steps,
+	} from '$lib/constants';
+	import {
+		Accordion,
+		AccordionGroup,
+		Button,
+		Card,
+		Error,
+		FeatureItem,
+		FormGroup,
+		Input,
+		Label,
+		LogoSlider,
+		Select,
+		Textarea,
+		Confetti,
+	} from '$lib/components/ui';
+
+	import { send_contact_data } from './index.remote';
+
+	let contact_success = $derived(send_contact_data?.result?.success ?? null);
+
+	$effect(() => {
+		if (contact_success === null)
+			return;
+
+		if (!contact_success) {
+			toast.error(send_contact_data?.result?.message!);
+			return;
+		}
+
+		toast.success(send_contact_data?.result?.message!);
+	});
 </script>
+
+<!-- User feedback on contact success -->
+<Confetti duration={4000} show={contact_success ?? false} />
+<Toaster position='bottom-center' />
 
 <!-- Inicio -->
 <section id='inicio' class='relative isolate'>
@@ -44,6 +86,7 @@
 			</hgroup>
 			<Button
 				class='w-full focus-within:ring-offset-primary hover:ring-offset-primary md:mx-auto md:w-fit'
+				href='#contacto'
 				size='lg'
 				variant='accent'
 			>
@@ -59,8 +102,10 @@
 			{#each hero_card_items as { image: Icon, title, description }}
 				<Card {description} {title}>
 					{#snippet image()}
-						<div class='w-fit rounded-md bg-muted-foreground p-2'>
-							<Icon class='size-8' />
+						<div
+							class='w-fit rounded-md border border-accent/15 bg-accent/10 p-2'
+						>
+							<Icon class='size-8 fill-accent' />
 						</div>
 					{/snippet}
 				</Card>
@@ -70,15 +115,13 @@
 </section>
 
 <!-- Servicios -->
-<section id='servicios' class='relative isolate'>
+<section id='servicios' class='relative isolate md:-scroll-m-8'>
 	<div
 		class='mx-auto max-w-(--content-width) px-(--content-x-spacing) pt-(--content-y-spacing) pb-10 lg:pb-16 xl:pb-26'
 	>
 		<div class='flex flex-col gap-y-20'>
 			<hgroup class='mx-auto text-center md:w-2/3'>
-				<h2
-					class='pb-6 text-3xl font-bold sm:text-4xl md:text-5xl'
-				>
+				<h2 class='pb-6 text-3xl font-bold sm:text-4xl md:text-5xl'>
 					Nuestros Servicios
 				</h2>
 				<p class='text-lg leading-snug text-balance text-muted'>
@@ -107,7 +150,7 @@
 </section>
 
 <!-- Como Trabajamos -->
-<section id='metodologia' class='relative isolate'>
+<section id='metodologia' class='relative isolate md:scroll-m-16'>
 	<div
 		class='mx-auto max-w-(--content-width) space-y-16 px-(--content-x-spacing) pt-(--content-y-spacing) pb-10 lg:pb-16 xl:pb-26'
 	>
@@ -193,7 +236,9 @@
 							{/snippet}
 
 							{#snippet content()}
-								<p class='text-lg leading-snug text-pretty text-muted'>
+								<p
+									class='text-lg leading-snug text-pretty text-muted'
+								>
 									{question.description}
 								</p>
 							{/snippet}
@@ -206,10 +251,111 @@
 </section>
 
 <!-- Contacto -->
-<section id='contacto' class='relative isolate'>
+<section id='contacto' class='relative isolate scroll-m-16 xl:scroll-m-22'>
 	<div
-		class='mx-auto max-w-(--content-width) px-(--content-x-spacing) pt-(--content-y-spacing) pb-10 md:gap-y-16 lg:pb-16 xl:pb-26'
+		class='mx-auto max-w-(--content-width) space-y-8 px-(--content-x-spacing) py-10 md:space-y-16 md:gap-y-16 lg:py-16 xl:py-26'
 	>
-		<!--  -->
+		<div class='rounded-2xl bg-background'>
+			<div
+				class='grid grid-rows-[auto_auto] place-items-center items-center gap-6 md:gap-10 xl:grid-cols-2 xl:gap-16'
+			>
+				<div class='space-y-16'>
+					<hgroup class='space-y-4'>
+						<h2
+							class='text-3xl leading-tight font-bold sm:text-4xl md:text-5xl'
+						>
+							¿Listo para transformar tu negocio con IA?
+						</h2>
+						<p
+							class='text-left leading-snug text-pretty text-muted'
+						>
+							Contáctanos hoy mismo y uno de nuestros
+							especialistas se comunicará contigo en menos de 24
+							horas para explorar cómo nuestras soluciones de
+							inteligencia artificial pueden automatizar tus
+							procesos, reducir costos operativos y liberarte para
+							enfocarte en lo que realmente importa para tu
+							negocio.
+						</p>
+					</hgroup>
+					<div class='space-y-10'>
+						{#each contact_items as item (item.title)}
+							<FeatureItem {...item} />
+						{/each}
+					</div>
+				</div>
+
+				<form
+					{...send_contact_data}
+					class='w-full rounded-2xl bg-muted-foreground p-6 shadow-md md:p-10 xl:p-16'
+				>
+					<div
+						class='grid grid-cols-2 justify-between gap-(--content-x-spacing)'
+					>
+						<FormGroup>
+							<Label for='name'>Nombre</Label>
+							<Input
+								name='name'
+								placeholder='Ingrese su nombre'
+							/>
+							{#if send_contact_data.result?.errors?.name}
+								<Error>{send_contact_data.result.errors.name}</Error>
+							{/if}
+						</FormGroup>
+
+						<FormGroup>
+							<Label for='email'>Correo electrónico</Label>
+							<Input
+								name='email'
+								placeholder='Ingrese su correo'
+								type='email'
+							/>
+							{#if send_contact_data.result?.errors?.email}
+								<Error>{send_contact_data.result.errors.email}</Error>
+							{/if}
+						</FormGroup>
+					</div>
+
+					<FormGroup>
+						<Label for='company_name'>Nombre de la empresa</Label>
+						<Input
+							name='company_name'
+							placeholder='Ingrese nombre de la empresa'
+						/>
+						{#if send_contact_data.result?.errors?.company_name}
+							<Error>{send_contact_data.result.errors.company_name}</Error>
+						{/if}
+					</FormGroup>
+
+					<FormGroup>
+						<Label for='company_size'>Tamaño de la empresa</Label>
+						<Select
+							name='company_size'
+							options={company_size_options}
+							placeholder='Seleccione tamaño de la empresa'
+						/>
+						{#if send_contact_data.result?.errors?.company_size}
+							<Error>{send_contact_data.result.errors.company_size}</Error>
+						{/if}
+					</FormGroup>
+
+					<FormGroup>
+						<Label for='details'>Detalles</Label>
+						<Textarea
+							name='details'
+							placeholder='¿En qué podemos ayudarte?'
+							rows={4}
+						/>
+						{#if send_contact_data.result?.errors?.details}
+							<Error>{send_contact_data.result.errors.details}</Error>
+						{/if}
+					</FormGroup>
+
+					<Button class='mt-4 w-full' type='submit' variant='accent'>
+						Contactar
+					</Button>
+				</form>
+			</div>
+		</div>
 	</div>
 </section>
