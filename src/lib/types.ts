718e5033b83330a8d923a98175598f1c0492d9e2
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-08-14 16:11:08 -0400

    feat: added SelectOptions as MaybePromise

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,6 +2,8 @@ import type { EnhancedImgAttributes } from '@sveltejs/enhanced-img';
 
 export type Awaitable<T> = T | Promise<T>;
 
+export type MaybePromise<T> = (T | Promise<T>);
+
 export interface ProxiedValue<T = any> { current: T }
 
 export interface NavItem {
@@ -10,3 +12,11 @@ export interface NavItem {
 }
 
 export type Picture = Exclude<EnhancedImgAttributes['src'], string>;
+
+export interface SelectOption {
+	label: string | null;
+	value: string | number | boolean | null;
+	selected?: boolean | undefined;
+}
+
+export type SelectOptions = SelectOption[];
